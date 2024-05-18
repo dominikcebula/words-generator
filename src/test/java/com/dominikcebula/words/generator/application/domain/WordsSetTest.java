74c3e9364424c43c4d4c716eff44f70e005a8933
Dominik Cebula <dominikcebula@gmail.com>
2024-05-18 16:22:31 +0200

    Adding tests

---
+++ src/test/java/com/dominikcebula/words/generator/application/domain/WordsSetTest.java
@@ -0,0 +1,69 @@
+package com.dominikcebula.words.generator.application.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WordsSetTest {
+    private static final WordValue WORD_1 = new WordValue("compliance");
+    private static final WordValue WORD_2 = new WordValue("scalability");
+    private static final WordValue WORD_3 = new WordValue("logistics");
+
+    private WordsSet wordsSet;
+
+    @BeforeEach
+    public void setUp() {
+        wordsSet = new WordsSet();
+    }
+
+    @Test
+    void shouldAddSingleWord() {
+        wordsSet.add(WORD_1);
+
+        assertThat(wordsSet.getWordValues())
+                .containsExactly(WORD_1);
+    }
+
+    @Test
+    void shouldAddMultipleWords() {
+        wordsSet.add(WORD_1);
+        wordsSet.add(WORD_2);
+        wordsSet.add(WORD_3);
+
+        assertThat(wordsSet.getWordValues())
+                .containsExactly(WORD_1, WORD_2, WORD_3);
+    }
+
+    @Test
+    void shouldBeEmptyByDefault() {
+        assertThat(wordsSet.getWordValues())
+                .isEmpty();
+    }
+
+    @Test
+    void shouldReturnEmptyLineValue() {
+        assertThat(wordsSet.toLineValue())
+                .isEmpty();
+    }
+
+    @Test
+    void shouldReturnSingleLineValue() {
+        wordsSet.add(WORD_1);
+
+        assertThat(wordsSet.toLineValue())
+                .isEqualTo("V compliance\n");
+    }
+
+    @Test
+    void shouldReturnMultipleLineValues() {
+        wordsSet.add(WORD_1);
+        wordsSet.add(WORD_2);
+        wordsSet.add(WORD_3);
+
+        assertThat(wordsSet.toLineValue())
+                .isEqualTo("V compliance\n" +
+                        "V scalability\n" +
+                        "V logistics\n");
+    }
+}
