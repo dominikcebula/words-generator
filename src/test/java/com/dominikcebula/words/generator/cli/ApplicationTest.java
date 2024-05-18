0dc3a7ea0141b5ade1b7fb36ea008dfde4eacb10
Dominik Cebula <dominikcebula@gmail.com>
2024-05-18 20:35:37 +0200

    Adding tests

--- src/test/java/com/dominikcebula/words/generator/cli/ApplicationTest.java
+++ src/test/java/com/dominikcebula/words/generator/cli/ApplicationTest.java
@@ -1,10 +1,14 @@
 package com.dominikcebula.words.generator.cli;
 
+import org.apache.commons.io.output.TeeOutputStream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
@@ -16,13 +20,21 @@ class ApplicationTest {
     @TempDir
     private Path tempDir;
 
-    private MockExitHandler exitHandler;
     private Application application;
+    private MockExitHandler exitHandler;
+
+    private ByteArrayOutputStream outputCapture;
+    private PrintStream systemOut;
 
     @BeforeEach
     public void setUp() {
-        exitHandler = new MockExitHandler();
-        application = new Application(exitHandler);
+        setUpApplication();
+        setUpCaptureOutput();
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        tearDownCaptureOutput();
     }
 
     @Test
@@ -32,8 +44,42 @@ class ApplicationTest {
 
         runWithArgs("generate-dataset -words-file-path " + wordsAlphaSamplePath + " -data-set-output-file-path " + datasetOutputPath);
 
+        assertProducedDataSetExists(datasetOutputPath);
         assertProducedDataSetContent(datasetOutputPath, getWordsAlphaSampleDataContent());
         assertExitCode(0);
+    }
+
+    @Test
+    void shouldQueryForPossibleWords() {
+        runWithArgs("query-words -data-set-file-path " + getWordsAlphaSampleDataPath() + " -characters abbdelr");
+
+        assertCapturedOutputContains(
+                """
+                        dabbler
+                        drabble
+                        rabbled
+                        """);
+        assertExitCode(0);
+    }
+
+    private void setUpApplication() {
+        exitHandler = new MockExitHandler();
+        application = new Application(exitHandler);
+    }
+
+    private void setUpCaptureOutput() {
+        outputCapture = new ByteArrayOutputStream();
+        systemOut = System.out;
+        systemOut.flush();
+        System.setOut(new PrintStream(new TeeOutputStream(
+                systemOut,
+                outputCapture
+        )));
+    }
+
+    private void tearDownCaptureOutput() throws IOException {
+        System.setOut(systemOut);
+        outputCapture.close();
     }
 
     private String getWordsAlphaSamplePath() {
@@ -58,9 +104,20 @@ class ApplicationTest {
         application.execute(arguments.split(" "));
     }
 
+    private void assertProducedDataSetExists(String datasetOutputPath) {
+        assertThat(Files.exists(Path.of(datasetOutputPath)))
+                .withFailMessage(() -> "Expected output dataset to exists under path: " + datasetOutputPath)
+                .isTrue();
+    }
+
     private void assertProducedDataSetContent(String datasetOutputPath, String expectedContent) throws IOException {
         assertThat(readString(Path.of(datasetOutputPath)))
                 .isEqualTo(expectedContent);
+    }
+
+    private void assertCapturedOutputContains(String expectedContent) {
+        assertThat(outputCapture.toString())
+                .contains(expectedContent);
     }
 
     private void assertExitCode(int expectedExitCode) {
