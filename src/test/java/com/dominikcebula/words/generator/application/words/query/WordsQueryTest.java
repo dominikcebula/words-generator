b0605b6b7afc33ac584ec84b5c972ba0568b6a67
Dominik Cebula <dominikcebula@gmail.com>
2024-05-18 17:04:09 +0200

    Adding tests

---
+++ src/test/java/com/dominikcebula/words/generator/application/words/query/WordsQueryTest.java
@@ -0,0 +1,72 @@
+package com.dominikcebula.words.generator.application.words.query;
+
+import com.dominikcebula.words.generator.application.domain.WordKey;
+import com.dominikcebula.words.generator.application.domain.WordValue;
+import com.dominikcebula.words.generator.application.domain.WordsMap;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WordsQueryTest {
+    @Test
+    void shouldReturnMultiplePossibleWords() {
+        WordsMap wordsMap = createWordsMap();
+        WordsQuery wordsQuery = new WordsQuery(wordsMap);
+
+        List<String> possibleWords = wordsQuery.queryPossibleWords("cgiilosst");
+
+        assertThat(possibleWords)
+                .containsExactly("logistics", "scalability", "compliance");
+    }
+
+    @Test
+    void shouldReturnSinglePossibleWord() {
+        WordsMap wordsMap = createWordsMap();
+        WordsQuery wordsQuery = new WordsQuery(wordsMap);
+
+        List<String> possibleWords = wordsQuery.queryPossibleWords("acehinsst");
+
+        assertThat(possibleWords)
+                .containsExactly("asthenics");
+    }
+
+    @Test
+    void shouldReturnNoPossibleWords() {
+        WordsMap wordsMap = createWordsMap();
+        WordsQuery wordsQuery = new WordsQuery(wordsMap);
+
+        List<String> possibleWords = wordsQuery.queryPossibleWords("abcxyz");
+
+        assertThat(possibleWords)
+                .isEmpty();
+    }
+
+    private WordsMap createWordsMap() {
+        WordsMap wordsMap = new WordsMap();
+
+        WordKey wordKey1 = new WordKey("1c1g2i1l1o2s1t");
+        WordValue wordValue11 = new WordValue("logistics");
+        WordValue wordValue12 = new WordValue("scalability");
+        WordValue wordValue13 = new WordValue("compliance");
+
+        WordKey wordKey2 = new WordKey("1a1c1e1h1i1n1s1t");
+        WordValue wordValue21 = new WordValue("asthenic");
+        WordValue wordValue22 = new WordValue("chanties");
+
+        WordKey wordKey3 = new WordKey("1a1c1e1h1i1n2s1t");
+        WordValue wordValue31 = new WordValue("asthenics");
+
+
+        wordsMap.add(wordKey1, wordValue11);
+        wordsMap.add(wordKey1, wordValue12);
+        wordsMap.add(wordKey1, wordValue13);
+
+        wordsMap.add(wordKey2, wordValue21);
+        wordsMap.add(wordKey2, wordValue22);
+
+        wordsMap.add(wordKey3, wordValue31);
+        return wordsMap;
+    }
+}
